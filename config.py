e8952356a44e35c6d63909fb7d21bd772739fd9f
Dilyorbek2005 <dilyorbekmirkomilov8@gmail.com>
2025-07-18 22:54:52 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")
-ADMINS = env.list("ADMINS")
+BOT_TOKEN = env.str("7683635500:AAFM56o7fZn-YDTywWH52B4xRMvjR173iMg")
+ADMINS = env.list("8048404748")
 WORK_DIRECTORY = env.str("WORK_DIRECTORY")
-API_ID = env.int("API_ID")
-API_HASH = env.str("API_HASH")
+API_ID = env.int("26860554")
+API_HASH = env.str("008aad4cabad032392bb5d83f93acb04")
 IP = env.str("ip")
