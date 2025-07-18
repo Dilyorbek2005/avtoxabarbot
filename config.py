ffbbc1669ca33e4ca8704e0eae93446849094a3a
Dilyorbek2005 <dilyorbekmirkomilov8@gmail.com>
2025-07-18 22:50:08 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = env.list("ADMINS")
+WORK_DIRECTORY = env.str("WORK_DIRECTORY")
+API_ID = env.int("API_ID")
+API_HASH = env.str("API_HASH")
+IP = env.str("ip")
